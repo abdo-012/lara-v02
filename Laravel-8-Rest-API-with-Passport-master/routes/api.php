d10f6c51a11d4023c9f6560b5062d3b868b4b138
Your name hereabdo-012 <zagabdelfatah07@gmail.com>
2021-06-24 16:34:00 +0100

    routes changes

--- Laravel-8-Rest-API-with-Passport-master/routes/api.php
+++ Laravel-8-Rest-API-with-Passport-master/routes/api.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\API\ProductController;
 
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
-   
+
 Route::middleware('auth:api')->group( function () {
     Route::resource('products', ProductController::class);
 });
